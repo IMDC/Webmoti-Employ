577729e50cc3355daac3be40bb41f0cf70490d0e
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-07-10 12:09:30 -0400

    server, shared: lint

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,6 +2,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   ignores: [
-    '**/db/schema.ts', '**/worker-configuration.d.ts', '**/.wrangler'
-  ]
+    '**/db/schema.ts',
+    '**/worker-configuration.d.ts',
+    '**/.wrangler',
+  ],
 })
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,15 +1,15 @@
 /* eslint-disable ts/no-redeclare */
-import { z } from "zod/v4";
+import { z } from 'zod/v4'
 
 export const InterviewInvite = z.object({
   id: z.number(),
   interviewId: z.number(),
   email: z.email(),
-});
+})
 export const NewInterviewInvite = InterviewInvite.omit({
   id: true,
   interviewId: true,
-});
+})
 
 export const Interview = z.object({
   id: z.number(),
@@ -18,13 +18,13 @@ export const Interview = z.object({
   endTime: z.coerce.date(),
   invites: z.array(InterviewInvite).optional(),
   sessionId: z.uuidv4(),
-});
+})
 export const NewInterview = Interview.omit({
   id: true,
   sessionId: true,
-});
+})
 
-export type Interview = z.infer<typeof Interview>;
-export type NewInterview = z.infer<typeof NewInterview>;
-export type InterviewInvite = z.infer<typeof InterviewInvite>;
-export type NewInterviewInvite = z.infer<typeof NewInterviewInvite>;
+export type Interview = z.infer<typeof Interview>
+export type NewInterview = z.infer<typeof NewInterview>
+export type InterviewInvite = z.infer<typeof InterviewInvite>
+export type NewInterviewInvite = z.infer<typeof NewInterviewInvite>
