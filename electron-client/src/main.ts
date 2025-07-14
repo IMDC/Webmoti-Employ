2bb36c97e9e5830e28d528bb533c80faa27aea46
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-07-14 15:56:14 -0400

    electron: load /client in main window

--- electron-client/src/main.ts
+++ electron-client/src/main.ts
@@ -7,6 +7,8 @@ if (started) {
   app.quit();
 }
 
+const isDev = !app.isPackaged
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -17,15 +19,15 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  if (isDev) {
+    // DEV: load vite dev server from /client
+    mainWindow.loadURL('http://localhost:5173')
+    mainWindow.webContents.openDevTools()
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    // PROD: load the built app
+    const dist = path.join(__dirname, '../../client/dist/index.html')
+    mainWindow.loadFile(dist)
   }
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
