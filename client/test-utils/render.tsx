fa4414b6b537c33d2062c468e94b9b207e6d1674
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-07-15 12:51:11 -0400

    updated tsconfigs

--- client/test-utils/render.tsx
+++ client/test-utils/render.tsx
@@ -1,9 +1,12 @@
+import type {
+  RenderResult,
+} from '@testing-library/react'
 import { MantineProvider } from '@mantine/core'
-import { render as testingLibraryRender } from '@testing-library/react'
+import { render as rtlRender } from '@testing-library/react'
 import { theme } from '../src/theme'
 
-export function render(ui: React.ReactNode) {
-  return testingLibraryRender(ui, {
+export function render(ui: React.ReactNode): RenderResult {
+  return rtlRender(ui, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
       <MantineProvider theme={theme}>{children}</MantineProvider>
     ),
