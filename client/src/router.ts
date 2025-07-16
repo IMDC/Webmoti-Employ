b3816945bc2a9d8e8165e062eaed3f397ce758dd
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-07-16 12:37:09 -0400

    electron: fix routing 404 error

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,7 +1,14 @@
-import { createRouter } from '@tanstack/react-router'
+import { createBrowserHistory, createHashHistory, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-export const router = createRouter({ routeTree, notFoundMode: 'root' })
+const isElectron = import.meta.env.VITE_IS_ELECTRON === 'true'
+
+// hash history is needed for electron since it loads from "file://"
+const history = isElectron
+  ? createHashHistory()
+  : createBrowserHistory()
+
+export const router = createRouter({ history, routeTree, notFoundMode: 'root' })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
