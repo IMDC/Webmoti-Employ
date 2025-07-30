83183a815224adbc5258e9e4d15cbefccabf2a1f
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-07-30 12:54:56 -0400

    server: add env check to postinstall script

--- server/scripts/postinstall.js
+++ server/scripts/postinstall.js
@@ -1,8 +1,17 @@
+/* eslint-disable node/prefer-global/process */
 import { execSync } from 'node:child_process'
 import { existsSync } from 'node:fs'
+import 'dotenv/config'
 
-if (!existsSync('worker-configuration.d.ts')) {
-  console.log('worker-configuration.d.ts not found, running cf-typegen...')
+// also run if undefined
+const shouldAlwaysRun = process.env.POSTINSTALL_CF_TYPEGEN !== 'false'
+
+if (shouldAlwaysRun) {
+  console.log('Postinstall always run is on')
+}
+
+if (shouldAlwaysRun || !existsSync('worker-configuration.d.ts')) {
+  console.log('Running postinstall cf-typegen')
   execSync('pnpm run cf-typegen', { stdio: 'inherit' })
 }
 else {
