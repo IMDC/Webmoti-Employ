d92c5547d1d42085dc3317af87fee8ef363df5dc
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-08-01 17:33:49 -0400

    client: clean up feedback area

--- client/src/features/interview/session/components/FeedbackArea.tsx
+++ client/src/features/interview/session/components/FeedbackArea.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Center, Group, Stack, Text } from '@mantine/core'
+import { Center, Group, Stack, Text } from '@mantine/core'
 import {
   IconEyeCheck,
   IconEyeClosed,
@@ -30,39 +30,31 @@ export function FeedbackArea() {
   const looking = feedback.find(f => f.feedbackType === 'lookingAtInterviewer')?.isActive
 
   return (
-    <Box w="100%" h="100%" px="md" py="xs">
-      <Card
-        withBorder
-        radius="xl"
-        h="100%"
-      >
-        <Center h="100%">
-          <Group justify="center" align="center" gap={70}>
-            <FeedbackIcon
-              iconActive={<IconEyeFilled size={24} />}
-              iconInactive={<IconEyeDotted size={24} />}
-              label="Fixation"
-              isActive={fixation}
-            />
-            <FeedbackIcon
-              iconActive={<IconEyeCheck size={24} />}
-              iconInactive={<IconEyeClosed size={24} />}
-              label="Looking at Interviewer"
-              isActive={looking}
-            />
-            <FeedbackIcon
-              iconActive={<IconMoodSad size={24} />}
-              iconInactive={<IconMoodSad size={24} />}
-              label="Bad"
-            />
-            <FeedbackIcon
-              iconActive={<IconMoodHappyFilled size={24} />}
-              iconInactive={<IconMoodHappyFilled size={24} />}
-              label="Happy person"
-            />
-          </Group>
-        </Center>
-      </Card>
-    </Box>
+    <Center h="100%">
+      <Group gap={70}>
+        <FeedbackIcon
+          iconActive={<IconEyeFilled size={24} />}
+          iconInactive={<IconEyeDotted size={24} />}
+          label="Fixation"
+          isActive={fixation}
+        />
+        <FeedbackIcon
+          iconActive={<IconEyeCheck size={24} />}
+          iconInactive={<IconEyeClosed size={24} />}
+          label="Looking at Interviewer"
+          isActive={looking}
+        />
+        <FeedbackIcon
+          iconActive={<IconMoodSad size={24} />}
+          iconInactive={<IconMoodSad size={24} />}
+          label="Bad"
+        />
+        <FeedbackIcon
+          iconActive={<IconMoodHappyFilled size={24} />}
+          iconInactive={<IconMoodHappyFilled size={24} />}
+          label="Happy person"
+        />
+      </Group>
+    </Center>
   )
 }
