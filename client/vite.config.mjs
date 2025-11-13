2615a437aa1ad1c55db108b0928b54babe471c1e
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-11-12 21:27:58 -0500

    add .env.electron for electron build

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,44 +1,49 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// eslint-disable-next-line node/prefer-global/process
-const isElectron = process.env.VITE_IS_ELECTRON === 'true'
+// mode is either production, development, or electron
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line node/prefer-global/process
+  const env = loadEnv(mode, process.cwd(), '')
 
-export default defineConfig({
-  // electron needs relative paths, but web needs absolute
-  base: isElectron ? './' : '/',
-  plugins: [
+  const isElectron = env.VITE_IS_ELECTRON === 'true'
+
+  return {
+    // electron needs relative paths, but web needs absolute
+    base: isElectron ? './' : '/',
+    plugins: [
     // '@tanstack/router-plugin' needs to be passed before '@vitejs/plugin-react'
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tsconfigPaths(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-    // exclude playwright tests in tests/
-    exclude: ['node_modules', 'dist', 'tests/**'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8787',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }),
+      react(),
+      tsconfigPaths(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.mjs',
+      // exclude playwright tests in tests/
+      exclude: ['node_modules', 'dist', 'tests/**'],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8787',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
+    resolve: {
+      alias: {
       // fix for tabler/icons-react slowing down vite dev (https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119)
       // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      },
     },
-  },
+  }
 })
