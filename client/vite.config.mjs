989e9320938e8903302750e3234e99c6c25d4e8c
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-11-13 12:00:23 -0500

    client: remove tabler icons workaround (fixed)

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -30,11 +30,4 @@ export default defineConfig({
       },
     },
   },
-  resolve: {
-    alias: {
-      // fix for tabler/icons-react slowing down vite dev (https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119)
-      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
-    },
-  },
 })
