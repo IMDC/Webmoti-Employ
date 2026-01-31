fead71dedbba574eaf55058cd285d7d5f7159c38
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2026-01-30 22:54:00 -0500

    style improvements

--- client/src/features/interview/ai/AudioProvider.tsx
+++ client/src/features/interview/ai/AudioProvider.tsx
@@ -7,6 +7,8 @@ import { RECORDING_SAMPLE_RATE } from './speechmatics-utils'
 export function AudioProvider({ children }: PropsWithChildren) {
   const audioContext = useMemo(() => new window.AudioContext({ sampleRate: RECORDING_SAMPLE_RATE }), [])
 
+  // TODO: cleanup the audioContext here. It shouldn't interfere with speechmatics in react strict mode.
+
   return (
     <PCMAudioRecorderProvider
       audioContext={audioContext}
--- shared/src/speechmatics.ts
+++ shared/src/speechmatics.ts
@@ -1,4 +1,4 @@
-import z from 'zod'
+import { z } from 'zod'
 
 export const SpeechmaticsResponse = z.object({ key: z.string() })
 export type SpeechmaticsResponse = z.infer<typeof SpeechmaticsResponse>
