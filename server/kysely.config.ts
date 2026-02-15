053c152ac552825187f0d996bedef5e168e64dc7
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2026-02-15 13:36:41 -0500

    separate better-auth migration

--- server/kysely.config.ts
+++ server/kysely.config.ts
@@ -1,25 +1,8 @@
-/* eslint-disable node/prefer-global/process */
 import { defineConfig } from 'kysely-ctl'
-import { getDb } from './src/db/getDb'
-
-const {
-  MIGRATION_TARGET,
-  WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE,
-  DATABASE_URL,
-} = process.env
-
-if (!MIGRATION_TARGET || !WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE || !DATABASE_URL)
-  throw new Error('Missing database .env variables')
-
-const migrationUrl = MIGRATION_TARGET === 'LOCAL'
-  ? WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE
-  : DATABASE_URL
-
-// run migrations on either local or remote database (depending on the set env variable)
-const db = getDb(migrationUrl)
+import { getMigrationDb } from './src/db/getMigrationDb'
 
 export default defineConfig({
-  kysely: db,
+  kysely: getMigrationDb(),
   migrations: {
     migrationFolder: 'src/db/migrations',
   },
