3778267eb14023df44272046f10a5026fdea6201
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2026-02-14 19:27:39 -0500

    add remote migration option

--- server/kysely.config.ts
+++ server/kysely.config.ts
@@ -1,15 +1,26 @@
+/* eslint-disable node/prefer-global/process */
 import { defineConfig } from 'kysely-ctl'
 import { getDb } from './src/db/getDb'
 
-// eslint-disable-next-line node/prefer-global/process
-const { WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE } = process.env
+const {
+  MIGRATION_TARGET,
+  WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE,
+  DATABASE_URL,
+} = process.env
 
-// run migrations on local database
-const db = getDb(WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE!)
+if (!MIGRATION_TARGET || !WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE || !DATABASE_URL)
+  throw new Error('Missing database .env variables')
+
+const migrationUrl = MIGRATION_TARGET === 'LOCAL'
+  ? WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE
+  : DATABASE_URL
+
+// run migrations on either local or remote database (depending on the set env variable)
+const db = getDb(migrationUrl)
 
 export default defineConfig({
   kysely: db,
   migrations: {
-    migrationFolder: 'src/migrations',
+    migrationFolder: 'src/migrations/db',
   },
 })
