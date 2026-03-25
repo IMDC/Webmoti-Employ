4047276ac371145785457367c0f5f885cb143e9c
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2026-03-24 20:51:50 -0400

    client: make dark mode higher contrast

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -2,6 +2,21 @@ import { createTheme, DEFAULT_THEME } from '@mantine/core'
 
 export const theme = createTheme({
   // mantine theme override
+  colors: {
+    // higher contrast dark theme: blacker backgrounds instead of default grayish
+    dark: [
+      '#C9C9C9',
+      '#B8B8B8',
+      '#828282',
+      '#696969',
+      '#4E4E4E',
+      '#353535',
+      '#161616',
+      '#0e0e0e',
+      '#080808',
+      '#020202',
+    ],
+  },
   breakpoints: {
     ...DEFAULT_THEME.breakpoints,
     // normal laptop size would be around 1536 pixels, so we can use this new xl breakpoint to detect large monitors
